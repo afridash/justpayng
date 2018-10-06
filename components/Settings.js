ba0613d5eb66733d6dd8b6e70e196db6ffb6471a
maybel66 <ogirikimabel@gmail.com>
2018-10-07 00:35:18 +0100

    Settings page done"

--- components/Settings.js
+++ components/Settings.js
@@ -1,7 +1,10 @@
-import React, {Component} from 'react'
-import {View, Text, StyleSheet} from 'react-native'
+import React, {Component} from 'react';
+import {View, Text, StyleSheet, Button, TouchableHighlight } from 'react-native';
+import { Container, Header, Content, Card, CardItem,Left, Right, Body, Title, } from "native-base";
+import Icon from 'react-native-vector-icons/FontAwesome';
+import {Actions} from 'react-native-router-flux'
 
-export default class Contacts extends Component {
+export default class Settings extends Component {
   constructor (props){
     super(props)
     this.state = {
@@ -10,20 +13,88 @@ export default class Contacts extends Component {
   }
   render () {
     return (
-      <View style={styles.container}>
-        <Text style={styles.text}>Hello World, this is Settings</Text>
-      </View>
+      <Container>
+
+        <Header>
+
+          <Body>
+            <Title>Settings</Title>
+          </Body>
+
+        </Header>
+        <Content>
+          <Card>
+
+            <CardItem>
+              <View style={styles.flatview}>
+                <Icon name="credit-card" size={30}  />
+                <View style={{marginLeft:28}}>
+                  <TouchableHighlight onPress={Actions.Bank} underlayColor='white' activeOpacity={0.7}>
+                  <Text style={styles.name}>Change account</Text>
+                    </TouchableHighlight>
+                </View>
+              </View>
+
+
+              {/* <Right> */}
+                <Icon style={styles.left} name="chevron-right" size={20}  />
+              {/* </Right> */}
+            </CardItem>
+
+          </Card>
+
+          <Card>
+
+            <CardItem>
+              <View style={styles.flatview}>
+                <Icon name="lock" size={40}  />
+                <View style={{marginLeft:35}}>
+                  <TouchableHighlight onPress={Actions.Bank} underlayColor='white' activeOpacity={0.7}>
+                  <Text style={styles.name}>Change password</Text>
+                  </TouchableHighlight>
+
+                </View>
+              </View>
+              {/* <Right> */}
+                <Icon style={styles.left} name="chevron-right" size={20}  />
+              {/* </Right> */}
+
+            </CardItem>
+          </Card>
+        </Content>
+      </Container>
+
+
+
     )
   }
 }
 
 const styles = StyleSheet.create({
   container:{
-    justifyContent:'center',
-    alignItems:'center'
+    flex: 1,
+    marginTop: 50,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
   },
   text:{
     fontSize:20,
     fontWeight:'600'
+  },
+  flatview: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    paddingTop: 20,
+    paddingBottom: 10,
+    borderRadius: 2,
+  },
+  name: {
+    fontFamily: 'Verdana',
+    fontSize: 18,
+    marginBottom: 10
+  },
+  left: {
+    marginLeft: 150,
   }
 })
